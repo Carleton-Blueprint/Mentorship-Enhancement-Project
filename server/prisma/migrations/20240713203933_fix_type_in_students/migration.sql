421e7806c78bdaa083a88ca3eb0494e2c47c6a25
Yufeng Liu <yufeng.liu15@gmail.com>
2024-07-22 10:02:12 -0400

    casing issue fixed

--- server/prisma/migrations/20240713203933_fix_type_in_students/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Student" ALTER COLUMN "student_id" SET DATA TYPE BIGINT;
--- server/src/controllers/student_controller.ts
+++ server/src/controllers/student_controller.ts
@@ -3,10 +3,18 @@ import { PrismaClient, Prisma } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const insertStudent = async (request: any, response: any) => {
-    const studentAdded = request.body.data;
-    console.log(studentAdded)
+    const studentCamelCase = request.body.data;
+    const studentJSON = convertKeysToSnakeCase(studentCamelCase)
+
+    // convert from string to int
+    studentJSON["student_id"] = Number(studentJSON["student_id"])
+
+    // placeholders below to prevent errors
+    delete studentJSON["courses"];
+    delete studentJSON["availability"];
+    console.log(studentJSON)
     try{
-        const createUser = await prisma.student.create({ data: studentAdded })
+        const createUser = await prisma.student.create({ data: studentJSON })
         response.status(201).json({message: 'Student has been created', createUser});
 
     } catch (error: any){
@@ -14,4 +22,24 @@ export const insertStudent = async (request: any, response: any) => {
     }
 }
 
-//function validateStudent
+function camelToSnakeCase(str) {
+    if (str == "entityNumber"){
+        return 'student_id';
+    }
+    return str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+}
+
+function convertKeysToSnakeCase(obj) {
+    if (Array.isArray(obj)) {
+        return obj.map(val => convertKeysToSnakeCase(val));
+    } else if (obj !== null && typeof obj === 'object') {
+        return Object.keys(obj).reduce(
+            (acc, key) => ({
+                ...acc,
+                [camelToSnakeCase(key)]: convertKeysToSnakeCase(obj[key])
+            }),
+            {}
+        );
+    }
+    return obj;
+}
