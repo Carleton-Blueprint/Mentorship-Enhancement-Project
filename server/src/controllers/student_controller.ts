8a4d1c59db362b110538a46727e72cbe0177a6e7
Yufeng Liu <yufeng.liu15@gmail.com>
2024-07-25 09:45:53 -0400

    created a simpler function for casing

--- server/src/controllers/student_controller.ts
+++ server/src/controllers/student_controller.ts
@@ -4,26 +4,33 @@ const prisma = new PrismaClient();
 
 export const insertStudent = async (request: any, response: any) => {
     const studentCamelCase = request.body.data;
-    const studentJSON = convertKeysToSnakeCase(studentCamelCase)
-
-    // convert from string to int
-    studentJSON["student_id"] = Number(studentJSON["student_id"])
-
-    // placeholders below to prevent errors
-    delete studentJSON["courses"];
-    delete studentJSON["availability"];
-    console.log(studentJSON)
-    try{
+    const studentJSON = convertToBackEndFormat(studentCamelCase)
+    
+    try {
         const createUser = await prisma.student.create({ data: studentJSON })
-        response.status(201).json({message: 'Student has been created', createUser});
-
-    } catch (error: any){
-        response.status(500).json({error: error.message});
+        response.status(201).json({ message: 'Student has been created', createUser });
+    } catch (error: any) {
+        response.status(500).json({ error: error.message });
     }
 }
 
+// Simple function to understand, but must be manually updated
+function convertToBackEndFormat(student) {
+    return {
+        "student_id": Number(student["entityNumber"]),
+        "first_name": student["firstName"],
+        "last_name": student["lastName"],
+        "major": student["major"],
+        "preferred_name": student["preferredName"],
+        "preferred_pronouns": student["preferredPronouns"],
+        "email": student["email"],
+        "year_level": student["yearLevel"]
+    }
+}
+
+// Below is more complicated, but technically more adaptable
 function camelToSnakeCase(str) {
-    if (str == "entityNumber"){
+    if (str == "entityNumber") {
         return 'student_id';
     }
     return str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
