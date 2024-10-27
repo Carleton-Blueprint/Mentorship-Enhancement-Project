57455452c078857fddc37bd68dda48f6a51be5fa
Rosa Li <rosali.tks@gmail.com>
2024-10-27 19:16:12 -0400

    Add vercel.json

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,13 @@
-// import express from "express";
-// import dotenv from "dotenv";
+import express from "express";
+import dotenv from "dotenv";
 import { app } from "./app";
 
-// dotenv.config();
-// const port = process.env.PORT || 5000;
+dotenv.config();
+const port = process.env.PORT || 5000;
 
 
-// app.listen(port, () => {
-//   console.log(`App is listening to port: ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`App is listening to port: ${port}`);
+});
 
 module.exports = app
