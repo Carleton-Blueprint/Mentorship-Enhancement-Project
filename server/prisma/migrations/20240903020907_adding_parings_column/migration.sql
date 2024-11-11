e601c9d87072f0325f43fac0fe4330c44f3ecaee
Rosa Li <rosali.tks@gmail.com>
2024-11-10 21:50:20 -0500

    dockerized project

--- server/prisma/migrations/20240903020907_adding_parings_column/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `Pairings` to the `Mentor` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Mentor" ADD COLUMN     "Pairings" INTEGER NOT NULL;
