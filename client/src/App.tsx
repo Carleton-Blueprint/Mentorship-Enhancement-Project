a17b78202741a4abc46dd9fbbf580dbea674a208
Rosa Li <rosali.tks@gmail.com>
2024-12-05 18:44:59 -0500

    reset user

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import { Login } from "./Login";
 import { Home } from "./Home";
 import { Toaster } from "./components/ui/toaster"
+import { FileSpreadsheetIcon } from "lucide-react";
 
 function App() {
   //default should be false
-  const [loggedIn, setLoggedIn] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div>
       <Toaster />
