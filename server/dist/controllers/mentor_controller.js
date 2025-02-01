12d5b7755c102bb36c2d5eb703cd60b8ad10103e
Rosa Li <rosa.li@shopify.com>
2025-01-31 19:09:40 -0500

    built files

--- server/dist/controllers/mentor_controller.js
+++ server/dist/controllers/mentor_controller.js
@@ -295,12 +295,11 @@ const addAvailability = async (mentor_data) => {
 };
 const callCreate = async (mentors) => {
     try {
-        console.log("Starting mentor creation with data:", mentors);
         // 1. Process all courses first
         const allCourses = new Set(mentors.flatMap((mentor) => mentor.courses || []));
-        console.log("Found courses:", Array.from(allCourses));
         // Create courses if they don't exist
         if (allCourses.size > 0) {
+            console.log("allCourses", allCourses);
             // Create courses and get their IDs in a single transaction
             const result = await prismaClient_1.default.$transaction(async (tx) => {
                 // Create courses
