09e8dce4835e99a9a791b852bd3924a66ea30910
Rosa Li <rosa.li@shopify.com>
2025-02-11 17:10:28 -0500

    set login to false

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import { FileSpreadsheetIcon } from "lucide-react";
 
 function App() {
   //default should be false
-  const [loggedIn, setLoggedIn] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div>
       <Toaster />
